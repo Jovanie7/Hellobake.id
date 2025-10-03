e0c4b1cab8310001fc3ce2fa76e18449f5001b4f
Jovanie7 <jovanie1028@gmail.com>
2025-10-03 13:28:06 +0700

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { label: "Small (20x10 cm)", price: 95000 },
             { label: "Large (20x20 cm)", price: 190000 }
           ],
-          img: "brownies.jpg",
+          img: "Picture/brownies.jpg",
           bestSeller: true
         },
         { 
@@ -462,4 +462,5 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
 
